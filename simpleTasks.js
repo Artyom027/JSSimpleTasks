a3366934da4cd9cefc2beb1aafa496e1eddb7a11
Your Name <you@example.com>
2021-01-27 17:02:06 +0400

    Added the task 2

--- simpleTasks.js
+++ simpleTasks.js
@@ -1,10 +1,11 @@
-
-
-function test1_isEven(n) {
+function task1_isEven(n) {
     return (0 == n % 2)
 }
 
+function task2_isOdd(n) {
+    return  n > 100 && !task1_isEven(n);
+}
 
 
-let x = test1_isEven(6)
+let x = task2_isOdd(6)
 console.log( x )
